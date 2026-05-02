f5530ea83fb95b7887f6313132bf9850fafb6428
Alla Mozharova <alik19.09@mail.ru>
2026-05-02 22:08:55 +0300

    [Feature] Add fonts

--- iOS-FakeNFT-Extended/DesignSystem/Fonts.swift
+++ iOS-FakeNFT-Extended/DesignSystem/Fonts.swift
@@ -1,19 +1,23 @@
 import UIKit
 
 extension UIFont {
-    // Ниже приведены примеры шрифтов, настоящие шрифты надо взять из фигмы
-
+    
     // Headline Fonts
     static var headline1 = UIFont.systemFont(ofSize: 34, weight: .bold)
-    static var headline2 = UIFont.systemFont(ofSize: 28, weight: .bold)
+    static var headline2 = UIFont.systemFont(ofSize: 32, weight: .bold)
     static var headline3 = UIFont.systemFont(ofSize: 22, weight: .bold)
-    static var headline4 = UIFont.systemFont(ofSize: 20, weight: .bold)
-
+    
+    // Title Fonts
+    static var titleBold = UIFont.systemFont(ofSize: 20, weight: .bold)
+    static var titleRegular = UIFont.systemFont(ofSize: 20, weight: .regular)
+    
     // Body Fonts
     static var bodyRegular = UIFont.systemFont(ofSize: 17, weight: .regular)
     static var bodyBold = UIFont.systemFont(ofSize: 17, weight: .bold)
-
+    static var bodySemibold = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    
     // Caption Fonts
-    static var caption1 = UIFont.systemFont(ofSize: 15, weight: .regular)
+    static var caption3 = UIFont.systemFont(ofSize: 15, weight: .regular)
     static var caption2 = UIFont.systemFont(ofSize: 13, weight: .regular)
+    static var caption1 = UIFont.systemFont(ofSize: 10, weight: .medium)
 }
