57aca1eec8d56c10f210ca0634b61c8507e758f7
Alla Mozharova <alik19.09@mail.ru>
2026-05-05 01:27:26 +0300

    [Feature] Add mocks for Nft

--- iOS-FakeNFT-Extended/Models/Network/Nft.swift
+++ iOS-FakeNFT-Extended/Models/Network/Nft.swift
@@ -7,3 +7,31 @@ struct Nft: Decodable, Sendable {
     let rating: Int
     let price: Double
 }
+
+// MARK: - Mocks
+
+extension Nft {
+    static let mock1 = Nft(
+        id: "1",
+        name: "April",
+        images: [],
+        rating: 3,
+        price: 1.5
+    )
+    
+    static let mock2 = Nft(
+        id: "2",
+        name: "Luna",
+        images: [],
+        rating: 5,
+        price: 2.25
+    )
+    
+    static let mock3 = Nft(
+        id: "3",
+        name: "Cherry",
+        images: [],
+        rating: 1,
+        price: 0.75
+    )
+}
