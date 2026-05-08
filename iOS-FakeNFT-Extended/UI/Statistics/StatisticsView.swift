eb8fd55af2d4deaffb5c9480171a044e6f0a39fb
miracle768 <L0bastoff.ilya@yandex.ru>
2026-05-08 22:44:49 +0300

    Add statistics view and tab

---
+++ iOS-FakeNFT-Extended/UI/Statistics/StatisticsView.swift
@@ -0,0 +1,20 @@
+//
+//  StatisticsView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Илья Лобастов on 08.05.2026.
+//
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Статистика")
+                .navigationTitle("Статистика")
+        }
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
