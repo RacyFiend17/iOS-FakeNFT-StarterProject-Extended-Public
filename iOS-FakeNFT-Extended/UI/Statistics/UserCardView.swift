c7c446bb2a213cd0192a98a2c4acfbc449367129
miracle768 <L0bastoff.ilya@yandex.ru>
2026-05-17 10:59:03 +0300

    Add user website button

--- iOS-FakeNFT-Extended/UI/Statistics/UserCardView.swift
+++ iOS-FakeNFT-Extended/UI/Statistics/UserCardView.swift
@@ -2,42 +2,65 @@ import SwiftUI
 
 struct UserCardView: View {
     let user: StatisticsUser
-
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             userInfoView
                 .padding(.top, 22)
-
+            
             Text(user.description)
                 .font(.system(size: 13, weight: .regular))
                 .foregroundStyle(Color.blackUniversalYP)
                 .lineSpacing(2)
                 .padding(.top, 20)
-
+            
+            websiteButton
+                .padding(.top, 28)
+            
             Spacer()
         }
         .padding(.horizontal, 16)
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         .background(Color.whiteUniversalYP)
         .navigationBarTitleDisplayMode(.inline)
+        
     }
 }
 
 // MARK: - Subviews
 
 private extension UserCardView {
+    
     var userInfoView: some View {
         HStack(spacing: 16) {
             avatarView
-
+            
             Text(user.name)
                 .font(.system(size: 22, weight: .bold))
                 .foregroundStyle(Color.blackUniversalYP)
-
+            
             Spacer()
         }
     }
-
+    
+    var websiteButton: some View {
+        Button {
+            
+        } label: {
+            Text("Перейти на сайт пользователя")
+                .font(.system(size: 13, weight: .regular))
+                .foregroundStyle(Color.blackUniversalYP)
+                .frame(maxWidth: .infinity)
+                .frame(height: 40)
+                .background(Color.whiteUniversalYP)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.blackUniversalYP, lineWidth: 1)
+                }
+        }
+        .disabled(user.websiteURL == nil)
+    }
+    
     @ViewBuilder
     var avatarView: some View {
         if let avatarURL = user.avatarURL {
@@ -56,7 +79,7 @@ private extension UserCardView {
                 .clipShape(Circle())
         }
     }
-
+    
     var placeholderAvatarImage: some View {
         Image(.userFotoStubYP)
             .renderingMode(.original)
